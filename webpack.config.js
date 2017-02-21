e0e37a3af475f1e4841d690065e93e2473328091
Михаил Килин <killingdruid@gmail.com>
2017-02-21 21:02:03 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+'use strict'
+let loaders = [
+    {test: /\.ts(x?)$/, loader: 'ts-loader'},
+    {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+    }, {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        loader: 'raw'
+    }
+]
+let BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+let webpack = require('webpack')
+module.exports = {
+    entry: ['./app/index.ts'],
+    output: {
+        filename: 'build.js',
+        path: 'dist'
+    },
+    resolve: {
+        root: __dirname,
+        extensions: ['', '.ts', '.js', '.json']
+    },
+    resolveLoader: {
+        modulesDirectories: ["node_modules"]
+    },
+    devtool: "inline-eval-cheap-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './app/index.html',
+            inject: 'body',
+            hash: true
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 8080,
+            server: {
+                baseDir: 'dist'
+            },
+            ui: false,
+            online: false,
+            notify: false
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery',
+            'window.jquery': 'jquery'
+        })
+    ],
+    module:{
+        loaders: loaders
+    }
+}
