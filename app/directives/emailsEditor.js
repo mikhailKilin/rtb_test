7226adebf107ba7e9ed359c0ffc3b6cc743bf301
Михаил Килин <killingdruid@gmail.com>
2017-02-23 20:37:15 +0500

    take out html from input directive

--- app/directives/emailsEditor.js
+++ app/directives/emailsEditor.js
@@ -1,32 +1,15 @@
 import EmailController from '../controllers/EmailController'
-
+import html from '../html/inputEmail.html'
 export const emailsEditor = () => {
-    return {
-        restrict: "E",
-        controller: EmailController,
-        controllerAs: 'ctrl',
-        template: `<div class ='email-container'
-                         ng-click="ctrl.setInputFocus($event)">
-                        <div ng-repeat="email in ctrl.emails track by $index">
-                            <div class="email-div">
-                                <div class="{{email.isValid ? 'email-name' : 'email-name invalid'}}">
-                                    <div class="email-under-div">{{email.name}}</div>
-                                </div>
-                                <div ng-click="ctrl.deleteEmailString($index)" class="close-button">&#xD7</div>
-                            </div>
-                        </div>
-                        <input id="inputFocus"
-                               type="text"
-                               class="email-string"
-                               ng-model="ctrl.emailString"
-                               ng-keyup="ctrl.addEmailWithEvent($event)"
-                               ng-blur="ctrl.onBlur($event)"
-                               placeholder="add more people..."/>
-                    </div>`,
-        scope: {
-            ctrl: "="
-        }
+  return {
+    restrict: "E",
+    controller: EmailController,
+    controllerAs: 'ctrl',
+    template: html,
+    scope: {
+      ctrl: "="
     }
+  }
 }
 
 export default emailsEditor
